49fd9b17f2ef88e74bd99a384c19a25f1d9db3c0
heyang03 <heyang03@bilibili.com>
2024-05-20 21:16:33 +0800

    init Demo

---
+++ src/TenDayPractice.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hy on 2024/5/20.
+//
+
+#include "TenDayPractice.h"
---
+++ src/TenDayPractice.h
@@ -0,0 +1,14 @@
+//
+// Created by hy on 2024/5/20.
+//
+
+#ifndef CPPDEMO_TENDAYPRACTICE_H
+#define CPPDEMO_TENDAYPRACTICE_H
+
+
+class TenDayPractice {
+
+};
+
+
+#endif //CPPDEMO_TENDAYPRACTICE_H
